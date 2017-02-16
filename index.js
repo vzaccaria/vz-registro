78800b6951658ecab4bb9d9171b037db01b87805
Vittorio Zaccaria <vittorio.zaccaria@gmail.com>
2017-02-16 10:02:35 +0100

    minor: some minor work

--- index.js
+++ index.js
@@ -18,48 +18,54 @@ var info = _require2.info;
 var error = _require2.error;
 
 var getOptions = function (doc) {
-    "use strict";
-    var o = $d(doc);
-    var help = $o("-h", "--help", false, o);
-    var user = $o("-u", "--user", undefined, o);
-    var password = $o("-p", "--password", undefined, o);
-    var registro = $o("-r", "--registro", undefined, o);
-    var file = $o("-f", "--file", undefined, o);
-    var status = o.status;
-    var upload = o.upload;
-    return {
-        help: help, user: user, password: password, registro: registro, file: file, status: status, upload: upload
-    };
+  "use strict";
+  var o = $d(doc);
+  var help = $o("-h", "--help", false, o);
+  var user = $o("-u", "--user", undefined, o);
+  var password = $o("-p", "--password", undefined, o);
+  var registro = $o("-r", "--registro", undefined, o);
+  var file = $o("-f", "--file", undefined, o);
+  var status = o.status;
+  var upload = o.upload;
+  return {
+    help: help,
+    user: user,
+    password: password,
+    registro: registro,
+    file: file,
+    status: status,
+    upload: upload
+  };
 };
 
 var main = function () {
-    $fs.readFileAsync(__dirname + "/docs/usage.md", "utf8").then(function (it) {
-        var _getOptions = getOptions(it);
+  $fs.readFileAsync(__dirname + "/docs/usage.md", "utf8").then(function (it) {
+    var _getOptions = getOptions(it);
 
-        var help = _getOptions.help;
-        var user = _getOptions.user;
-        var password = _getOptions.password;
-        var registro = _getOptions.registro;
-        var file = _getOptions.file;
-        var status = _getOptions.status;
-        var upload = _getOptions.upload;
+    var help = _getOptions.help;
+    var user = _getOptions.user;
+    var password = _getOptions.password;
+    var registro = _getOptions.registro;
+    var file = _getOptions.file;
+    var status = _getOptions.status;
+    var upload = _getOptions.upload;
 
-        if (help) {
-            console.log(it);
-        } else if (status) {
-            var cmd = "casperjs " + __dirname + "/lib/_status.js --username=" + user + " --password=" + password + " --registro=" + registro;
-            warn(cmd);
-            $s.execAsync(cmd).then(function () {
-                info("file saved in status.png");
-            });
-        } else if (upload) {
-            var cmd = "casperjs " + __dirname + "/lib/_upload.js --username=" + user + " --password=" + password + " --registro=" + registro + " --datafile=" + file;
-            warn(cmd);
-            $s.execAsync(cmd).then(function () {
-                info("finished uploading");
-            });
-        }
-    });
+    if (help) {
+      console.log(it);
+    } else if (status) {
+      var cmd = "casperjs " + __dirname + "/lib/_status.js --username=" + user + " --password=" + password + " --registro=" + registro;
+      warn(cmd);
+      $s.execAsync(cmd).then(function () {
+        info("file saved in status.png");
+      });
+    } else if (upload) {
+      var cmd = "casperjs " + __dirname + "/lib/_upload.js --username=" + user + " --password=" + password + " --registro=" + registro + " --datafile=" + file;
+      warn(cmd);
+      $s.execAsync(cmd).then(function () {
+        info("finished uploading");
+      });
+    }
+  });
 };
 
 main();
--- src/_messages.js
+++ src/_messages.js
@@ -1,27 +1,27 @@
-let color = require('../proxies/chalk').set
+let color = require("../proxies/chalk").set;
 
 let chalk = {
-    yellow: function(s) {
-        return color(s, "yellow")
-    },
-    blue: function(s) {
-        return color(s, "blue")
-    },
-    red: function(s) {
-        return color(s, "red")
-    }
-}
+  yellow: function(s) {
+    return color(s, "yellow");
+  },
+  blue: function(s) {
+    return color(s, "blue");
+  },
+  red: function(s) {
+    return color(s, "red");
+  }
+};
 
 function warn(msg) {
-        console.error(chalk.yellow('WARN: ') + msg);
+  console.error(chalk.yellow("WARN: ") + msg);
 }
 
 function info(msg) {
-        console.error(chalk.blue('INFO: ') + msg);
+  console.error(chalk.blue("INFO: ") + msg);
 }
 
 function error(msg) {
-        console.error(chalk.red(' ERR: ') + msg);
+  console.error(chalk.red(" ERR: ") + msg);
 }
 
-module.exports = { warn, info, error }
+module.exports = { warn, info, error };
